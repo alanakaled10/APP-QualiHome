c48b4b921082185dfa7106428179a0f3906c7ada
AlanaKaled1 <alanakaled1@gmail.com>
2025-11-27 16:37:09 -0300

    Perfíl adicionado

--- QualiHome/js/script.js
+++ QualiHome/js/script.js
@@ -32,8 +32,57 @@ document.querySelectorAll('.menu-item').forEach(item => {
 });
 
 let btn_ia = document.querySelector(".btn_ia");
+let btn_chamado = document.querySelector(".btn_chamado");
 
 btn_ia.addEventListener('click', function () {
     window.location.href = "scan.html";
 })
+
+btn_chamado.addEventListener('click', function () {
+    window.location.href = "chamado.html";
+})
+
+
+
+
+function goHome() {
+    window.location.href = 'home.html';
+}
+
+function goChamado() {
+    window.location.href = 'chamado.html';
+}
+
+function goPerfil() {
+    window.location.href = 'perfil.html';
+}
+
+// Função para selecionar o tipo de chamado (Solicitação, Problema, Dúvida)
+function selecionarTipo(elementoClicado) {
+    // 1. Remove a classe 'active' de todos os chips
+    const todosChips = document.querySelectorAll('.chip');
+    todosChips.forEach(chip => {
+        chip.classList.remove('active');
+    });
+
+    // 2. Adiciona a classe 'active' apenas no que foi clicado
+    elementoClicado.classList.add('active');
+}
+
+// Função para mostrar o nome da foto quando o usuário escolhe um arquivo
+function atualizarNomeFoto() {
+    const input = document.getElementById('inputFoto');
+    const texto = document.getElementById('textoFoto');
+
+    // Verifica se algum arquivo foi selecionado
+    if (input.files && input.files.length > 0) {
+        // Pega o nome do primeiro arquivo
+        const nomeArquivo = input.files[0].name;
+
+        // Muda o texto do botão e a cor para indicar sucesso
+        texto.innerText = nomeArquivo;
+        texto.style.color = "#00c7a5";
+        texto.style.fontWeight = "bold";
+    }
+}
 
