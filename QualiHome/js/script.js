1e514201c18b92288e80b5b082d755ca60b25a9d
AlanaKaled1 <alanakaled1@gmail.com>
2025-11-27 09:53:32 -0300

    aperfeiçoamento da tela home

--- QualiHome/js/script.js
+++ QualiHome/js/script.js
@@ -7,3 +7,27 @@ function continuar() {
         window.location.href = "home.html";
     }
 }
+
+
+// Script simples para simular interação nos botões
+document.querySelectorAll('.btn-action, .btn-detalhes').forEach(btn => {
+    btn.addEventListener('click', () => {
+        console.log('Botão clicado');
+        // Animação simples de clique
+        btn.style.transform = "scale(0.95)";
+        setTimeout(() => {
+            btn.style.transform = "scale(1)";
+        }, 100);
+    });
+});
+
+document.querySelectorAll('.menu-item').forEach(item => {
+    item.addEventListener('click', function () {
+        document.querySelectorAll('.menu-item').forEach(i => i.classList.remove('active-menu'));
+        document.querySelectorAll('.menu-indicator').forEach(ind => ind.style.display = 'none');
+
+        this.classList.add('active-menu');
+        this.querySelector('.menu-indicator').style.display = 'block';
+    });
+});
+
